b84e0645c568699921b30c26f8fd0e76212b0c9e
Dmitry <medtex03@gmail.com>
2020-12-22 17:15:16 +0800

    Code reconstruction

--- Form4.cs
+++ Form4.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using pulse.collection;
+using System;
 using System.Data;
 using System.Data.SqlClient;
 using System.Windows.Forms;
@@ -8,43 +9,38 @@ namespace pulse
 {
     public partial class Form4 : Form
     {
-        public Form4()
+        private Patient _patient;
+        public Patient patient { get => _patient; set => _patient = value; }
+
+        public Form4(Patient patient = null)
         {
             InitializeComponent();
-        }
-        public String id;
-        public string ident
-        {
-            get { return id; }
-            set { id = value; }
+            _patient = patient;
         }
 
         private void textBox4_KeyPress(object sender, KeyPressEventArgs e)
-        {
-            char number = e.KeyChar;
-            if (!Char.IsDigit(number) && number != 8) // цифры и клавиша BackSpace
-            {
-                e.Handled = true;
-            }
+        {   // цифры и клавиша BackSpace (8)
+            if (!Char.IsDigit(e.KeyChar) && e.KeyChar != 8) { e.Handled = true; }
         }
 
         private void Form4_Load(object sender, EventArgs e)
         {
-            //String id = ident.Value;
-            Console.WriteLine(id);
-            // label5.Text = "Id = " + id;
-              sqlConnection = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename='|DataDirectory|\Database1.mdf';Integrated Security=True");
-           // sqlConnection = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\Database1.mdf;Integrated Security=True");
-           // sqlConnection = new SqlConnection(@" Data Source = (LocalDB)\MSSQLLocalDB; AttachDbFilename = C:\Users\Иван\Desktop\ДипР\14.05\pulse_2.0\Database1.mdf; Integrated Security = True");
+            if(_patient != null)
+            {
+                textBox1.Text = _patient.surname;
+                textBox2.Text = _patient.name;
+                textBox3.Text = _patient.middleName;
+                maskedTextBox1.Text = _patient.birthdayDate.ToString();
+                textBox5.Text = _patient.height.ToString();
+                textBox6.Text = _patient.weight.ToString();
+                comboBox1.Text = _patient.genderName();
+            }
             comboBox1.Items.AddRange(new string[] { "Мужской", "Женский" });
             label4.Text = "Дата рождения"+ "\r" + "(ММ.ДД.ГГГГ)";
         }
-        public SqlConnection sqlConnection = null;
         private void button1_Click(object sender, EventArgs e)
         {
-            //  sqlConnection = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename='|DataDirectory|\Database1.mdf';Integrated Security=True");
 
-            //  sqlConnection = new SqlConnection(@" Data Source = (LocalDB)\MSSQLLocalDB; AttachDbFilename = C:\Users\Иван\Desktop\ДипР\14.05\pulse_2.0\Database1.mdf; Integrated Security = True");
             if (!string.IsNullOrEmpty(textBox1.Text) && !string.IsNullOrWhiteSpace(textBox1.Text) &&
                  !string.IsNullOrEmpty(textBox2.Text) && !string.IsNullOrWhiteSpace(textBox2.Text) &&
                  !string.IsNullOrEmpty(textBox3.Text) && !string.IsNullOrWhiteSpace(textBox3.Text) &&
@@ -53,48 +49,40 @@ namespace pulse
                  !string.IsNullOrEmpty(textBox6.Text) && !string.IsNullOrWhiteSpace(textBox6.Text) &&
                  !string.IsNullOrEmpty(comboBox1.Text) && !string.IsNullOrWhiteSpace(comboBox1.Text))
             {
-                if (label5.Text == "")
+                if (_patient == null)
                 {
-                    sqlConnection.Open();
-                    SqlCommand command = new SqlCommand("INSERT INTO [TABLE] (Фамилия, Имя, Отчество, Дата_рождения, Рост, Вес, Пол) VALUES(@Фамилия, @Имя, @Отчество, @Дата_рождения, @Рост, @Вес, @Пол)", sqlConnection);
-                    command.Parameters.AddWithValue("Фамилия", textBox1.Text);
-                    command.Parameters.AddWithValue("Имя", textBox2.Text);
-                    command.Parameters.AddWithValue("Отчество", textBox3.Text);
-                    command.Parameters.AddWithValue("Дата_рождения", maskedTextBox1.Text);
-                    command.Parameters.AddWithValue("Рост", textBox5.Text);
-                    command.Parameters.AddWithValue("Вес", textBox6.Text);
-                    command.Parameters.AddWithValue("Пол", comboBox1.Text);
-                    string sqlFormattedDate = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
-
                     try
                     {
-                        command.ExecuteNonQuery();
-                        sqlConnection.Close();
+                        _patient = new Patient(
+                            textBox1.Text,
+                            textBox2.Text,
+                            textBox3.Text,
+                            comboBox1.Text == "Мужской",
+                            DateTime.Parse(maskedTextBox1.Text),
+                            Convert.ToInt32(textBox5.Text),
+                            Convert.ToInt32(textBox6.Text)
+                        );
+
+                        _patient.create();
+                    }
+                    catch { MessageBox.Show("Ошибка, проверьте данные!"); }
+                    finally {
                         MessageBox.Show("Запись сохранена.");
                         this.Close();
-                    }
-                    catch { MessageBox.Show("Ошибка, проверьте данные!");
-                        sqlConnection.Close();
                     }
                 }
                 else
                 {
-                    sqlConnection.Open();
-                    int r = Convert.ToInt32(label5.Text);
-                    SqlCommand update = new SqlCommand("UPDATE [TABLE] SET [Фамилия] = @Фамилия, [Имя] = @Имя, [Отчество] = @Отчество, [Дата_рождения] = @Дата_рождения, [Рост] = @Рост, [Вес] = @Вес, [Пол] = @Пол WHERE Id = @r", sqlConnection);
-                    //  SqlCommand update = new SqlCommand("UPDATE [TABLE] SET [Имя] = @Имя WHERE Id = 19", sqlConnection);
-                    // var cmd = new SqlCommand("UPDATE Table SET [Фамилия]=@Фамилия WHERE id =19");
-                    update.Parameters.AddWithValue("r", r);
-                    update.Parameters.AddWithValue("Фамилия", textBox1.Text);
-                    update.Parameters.AddWithValue("Имя", textBox2.Text);
-                    update.Parameters.AddWithValue("Отчество", textBox3.Text);
-                    update.Parameters.AddWithValue("Дата_рождения", maskedTextBox1.Text);
-                    update.Parameters.AddWithValue("Рост", textBox5.Text);
-                    update.Parameters.AddWithValue("Вес", textBox6.Text);
-                    update.Parameters.AddWithValue("Пол", comboBox1.Text);
 
-                    update.ExecuteNonQuery();
-                    sqlConnection.Close();
+                    _patient.surname = textBox1.Text;
+                    _patient.name = textBox2.Text;
+                    _patient.middleName = textBox3.Text;
+                    _patient.birthdayDate = DateTime.Parse(maskedTextBox1.Text);
+                    _patient.gender = comboBox1.Text == "Мужской";
+                    _patient.height = Convert.ToInt32(textBox5.Text);
+                    _patient.weight = Convert.ToInt32(textBox6.Text);
+
+                    _patient.update();
 
                     MessageBox.Show("Запись сохранена.");
                     this.Close();
