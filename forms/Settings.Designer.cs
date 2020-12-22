0d314b589fd1ccc170787b7fdd9d8eaa16c4f146
Dmitry <medtex03@gmail.com>
2020-12-22 17:13:48 +0800

    Settings form created

---
+++ forms/Settings.Designer.cs
@@ -0,0 +1,209 @@
+﻿
+namespace pulse.forms
+{
+    partial class Settings
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Settings));
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dbPath = new System.Windows.Forms.TextBox();
+            this.dbBtn = new System.Windows.Forms.Button();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.saveBtn = new System.Windows.Forms.Button();
+            this.resetBtn = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.savesPath = new System.Windows.Forms.TextBox();
+            this.savesBtn = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 4;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.79791F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 66.20209F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 36F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 39F));
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.dbPath, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.dbBtn, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 1, 5);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.savesPath, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.savesBtn, 2, 3);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 7;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 57.14286F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 42.85714F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 26F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 272F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 34F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 29F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(363, 444);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Location = new System.Drawing.Point(3, 36);
+            this.label1.Margin = new System.Windows.Forms.Padding(3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Путь к БД:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // dbPath
+            // 
+            this.dbPath.Location = new System.Drawing.Point(100, 36);
+            this.dbPath.Name = "dbPath";
+            this.dbPath.Size = new System.Drawing.Size(184, 20);
+            this.dbPath.TabIndex = 1;
+            // 
+            // dbBtn
+            // 
+            this.dbBtn.AutoSize = true;
+            this.dbBtn.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dbBtn.Location = new System.Drawing.Point(287, 33);
+            this.dbBtn.Margin = new System.Windows.Forms.Padding(0);
+            this.dbBtn.Name = "dbBtn";
+            this.dbBtn.Size = new System.Drawing.Size(36, 24);
+            this.dbBtn.TabIndex = 2;
+            this.dbBtn.Text = "...";
+            this.dbBtn.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.dbBtn.UseVisualStyleBackColor = true;
+            this.dbBtn.Click += new System.EventHandler(this.dbBtn_Click);
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 3;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 16F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 83F));
+            this.tableLayoutPanel2.Controls.Add(this.saveBtn, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.resetBtn, 2, 0);
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(100, 383);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(184, 28);
+            this.tableLayoutPanel2.TabIndex = 3;
+            // 
+            // saveBtn
+            // 
+            this.saveBtn.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.saveBtn.Location = new System.Drawing.Point(3, 3);
+            this.saveBtn.Name = "saveBtn";
+            this.saveBtn.Size = new System.Drawing.Size(79, 22);
+            this.saveBtn.TabIndex = 0;
+            this.saveBtn.Text = "Сохранить";
+            this.saveBtn.UseVisualStyleBackColor = true;
+            this.saveBtn.Click += new System.EventHandler(this.saveBtn_Click);
+            // 
+            // resetBtn
+            // 
+            this.resetBtn.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.resetBtn.Location = new System.Drawing.Point(104, 3);
+            this.resetBtn.Name = "resetBtn";
+            this.resetBtn.Size = new System.Drawing.Size(77, 22);
+            this.resetBtn.TabIndex = 1;
+            this.resetBtn.Text = "Сбросить";
+            this.resetBtn.UseVisualStyleBackColor = true;
+            this.resetBtn.Click += new System.EventHandler(this.resetBtn_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label2.Location = new System.Drawing.Point(3, 85);
+            this.label2.Margin = new System.Windows.Forms.Padding(3);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Путь к записям:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // savesPath
+            // 
+            this.savesPath.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.savesPath.Location = new System.Drawing.Point(100, 85);
+            this.savesPath.Name = "savesPath";
+            this.savesPath.Size = new System.Drawing.Size(184, 20);
+            this.savesPath.TabIndex = 5;
+            // 
+            // savesBtn
+            // 
+            this.savesBtn.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.savesBtn.Location = new System.Drawing.Point(287, 82);
+            this.savesBtn.Margin = new System.Windows.Forms.Padding(0);
+            this.savesBtn.Name = "savesBtn";
+            this.savesBtn.Size = new System.Drawing.Size(36, 26);
+            this.savesBtn.TabIndex = 6;
+            this.savesBtn.Text = "...";
+            this.savesBtn.UseVisualStyleBackColor = true;
+            this.savesBtn.Click += new System.EventHandler(this.savesBtn_Click);
+            // 
+            // Settings
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(369, 450);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Settings";
+            this.Padding = new System.Windows.Forms.Padding(3);
+            this.Text = "Настройки";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Settings_FormClosing);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox dbPath;
+        private System.Windows.Forms.Button dbBtn;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Button saveBtn;
+        private System.Windows.Forms.Button resetBtn;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox savesPath;
+        private System.Windows.Forms.Button savesBtn;
+    }
+}
---
+++ forms/Settings.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Windows.Forms;
+using pulse.core;
+
+namespace pulse.forms
+{
+    public partial class Settings : Form
+    {
+        public static int UNCHANGED = 0;
+        public static int CHANGED_SAVED = 1;
+        public static int CHANGED_NOT_SAVED = 2;
+
+        private int _state = UNCHANGED;
+
+        public Settings()
+        {
+            InitializeComponent();
+            dbPath.Text = Properties.Settings.Default.DBPath;
+            savesPath.Text = Properties.Settings.Default.savesPath;
+        }
+
+        /*  Additional functions */
+        private string GetFilePath()
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "Базы Данных Microsoft SQL Server (*.mdf)|*.mdf|All files (*.*)|*.*";
+                openFileDialog.FilterIndex = 1;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK) return openFileDialog.FileName;
+                else return null;
+            }
+        }
+        private string GetDirPath()
+        {
+            using (FolderBrowserDialog fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+
+                if (result == DialogResult.OK &&
+                    !string.IsNullOrWhiteSpace(fbd.SelectedPath)) return fbd.SelectedPath;
+
+                else return null;
+            }
+        }
+
+        /*  Events functions    */
+        private void dbBtn_Click(object sender, EventArgs e)
+        {
+            string filePath = GetFilePath();
+            if (filePath == null) return;
+
+            string connString = String.Format(DBconnection.connString, filePath);
+
+            try { new DBconnection(connString); }
+            catch(Exception exp) {
+                MessageBox.Show("Ошибка. Невозможно подключится к БД.\n Подробнее: " + exp.Message);
+            }
+            finally {
+                dbPath.Text = filePath;
+                Properties.Settings.Default.DBPath = filePath;
+                MessageBox.Show("База данных успешно подключена");
+                _state = CHANGED_NOT_SAVED;
+            }
+
+        }
+
+        private void saveBtn_Click(object sender, EventArgs e)
+        {
+            _state = CHANGED_SAVED;
+            Properties.Settings.Default.Save();
+            MessageBox.Show("Настройки успешно сохранены");
+        }
+
+        private void resetBtn_Click(object sender, EventArgs e)
+        {
+            _state = CHANGED_NOT_SAVED;
+            Properties.Settings.Default.Reset();
+            dbPath.Text = Properties.Settings.Default.DBPath;
+        }
+
+        private void Settings_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if(_state == CHANGED_NOT_SAVED)
+            {
+                var answer = MessageBox.Show("Вы хотите сохранить изменения ?", "Предупреждение",
+                         MessageBoxButtons.YesNo);
+
+                if (answer == DialogResult.Yes)
+                    Properties.Settings.Default.Save();
+                if (answer == DialogResult.No)
+                    e.Cancel = true;
+            }
+        }
+
+        private void savesBtn_Click(object sender, EventArgs e)
+        {
+            string dirPath = GetDirPath();
+            if (dirPath == null) return;
+            savesPath.Text = dirPath;
+            Properties.Settings.Default.savesPath = dirPath;
+        }
+    }
+}
