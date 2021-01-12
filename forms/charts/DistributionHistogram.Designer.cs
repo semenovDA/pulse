3a89d923aa24ae2236e14095227906958fa5401e
Dmitry <medtex03@gmail.com>
2021-01-12 16:34:21 +0800

    Interval distribution histogram added, with gaussian bell spline #6

---
+++ forms/charts/DistributionHistogram.Designer.cs
@@ -0,0 +1,81 @@
+﻿
+namespace pulse.forms.charts
+{
+    partial class DistributionHistogram
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DistributionHistogram));
+            this.Histogram = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.Histogram)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Histogram
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.Histogram.ChartAreas.Add(chartArea1);
+            this.Histogram.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Histogram.Location = new System.Drawing.Point(0, 0);
+            this.Histogram.Name = "Histogram";
+            series1.ChartArea = "ChartArea1";
+            series1.Name = "Series1";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series2.Name = "Series2";
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series3.Name = "Series3";
+            this.Histogram.Series.Add(series1);
+            this.Histogram.Series.Add(series2);
+            this.Histogram.Series.Add(series3);
+            this.Histogram.Size = new System.Drawing.Size(643, 440);
+            this.Histogram.TabIndex = 0;
+            this.Histogram.Text = "chart1";
+            // 
+            // DistributionHistogram
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(643, 440);
+            this.Controls.Add(this.Histogram);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "DistributionHistogram";
+            this.Text = "Гистограмма распределения";
+            ((System.ComponentModel.ISupportInitialize)(this.Histogram)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart Histogram;
+    }
+}
---
+++ forms/charts/DistributionHistogram.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MathNet.Numerics.Statistics;
+using MathNet.Numerics.Distributions;
+
+namespace pulse.forms.charts
+{
+
+    public partial class DistributionHistogram : Form
+    {
+
+        private List<PointF> NormalDistribution(IEnumerable<double> points, bool sample = false)
+        {
+            double mean = points.Average();
+            double std = Statistics.StandardDeviation(points);
+            double var = std * std;
+
+            List<PointF> n_points = new List<PointF>();
+            Normal dist = new Normal(mean, std);
+
+            foreach (var p in points.Distinct()) {
+                n_points.Add(new PointF((float)p, (float)dist.Density(p)));
+            }
+
+            return n_points.OrderBy(p => p.X).ToList();
+        }
+
+        private void FillValues(IEnumerable<double> points)
+        {
+
+            foreach (var u in points.Distinct()) {
+                Histogram.Series[0].Points.AddXY(u, points.Where(p => p == u).Count());
+            }
+
+            foreach (var p in NormalDistribution(points)) {
+                Histogram.Series[1].Points.AddXY(p.X, (p.Y * points.Count()));
+            }
+
+        }
+
+        public DistributionHistogram(IEnumerable<double> points)
+        {
+            InitializeComponent();
+            
+            // Settings
+            Histogram.Series[1].Color = Color.Red;
+            Histogram.Series[1].BorderWidth = 2;
+
+            Histogram.Series[2].Color = Color.Blue;
+            Histogram.Series[2].BorderWidth = 3;
+
+            // 
+            FillValues(points);
+        }
+    }
+
+}
