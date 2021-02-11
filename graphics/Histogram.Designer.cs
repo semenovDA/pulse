f0ad1228c98eacbf62d16170caf4e429ab60809d
Dmitry <medtex03@gmail.com>
2021-02-11 10:54:56 +0800

    Histogram component added

---
+++ graphics/Histogram.Designer.cs
@@ -0,0 +1,78 @@
+﻿
+namespace pulse.graphics
+{
+    partial class Histogram
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором компонентов
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chart)).BeginInit();
+            // 
+            // chart
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart.ChartAreas.Add(chartArea1);
+            this.chart.Dock = System.Windows.Forms.DockStyle.Fill;
+            legend1.Name = "Legend1";
+            this.chart.Legends.Add(legend1);
+            this.chart.Location = new System.Drawing.Point(0, 0);
+            this.chart.Name = "chart";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            series2.BorderWidth = 2;
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series2.Color = System.Drawing.Color.Red;
+            series2.Legend = "Legend1";
+            series2.Name = "Series2";
+            series3.BorderWidth = 3;
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series3.Color = System.Drawing.Color.LightBlue;
+            series3.Legend = "Legend1";
+            series3.Name = "Series3";
+            this.chart.Series.Add(series1);
+            this.chart.Series.Add(series2);
+            this.chart.Series.Add(series3);
+            this.chart.Size = new System.Drawing.Size(300, 300);
+            this.chart.TabIndex = 0;
+            this.chart.Text = "chart1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart)).EndInit();
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.DataVisualization.Charting.Chart chart;
+    }
+}
---
+++ graphics/Histogram.cs
@@ -0,0 +1,56 @@
+﻿using MathNet.Numerics.Statistics;
+using pulse.collection;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using MathNet.Numerics.Distributions;
+
+namespace pulse.graphics
+{
+    public partial class Histogram : Component
+    {
+        public Histogram(Signal signal)
+        {
+            InitializeComponent();
+            FillValues(signal.computeRR(false));
+        }
+
+        public Histogram(IContainer container, Signal signal)
+        {
+            container.Add(this);
+            InitializeComponent();
+            FillValues(signal.computeRR(false));
+        }
+
+        private void FillValues(List<double> points)
+        {
+            foreach (var u in points.Distinct())
+            {
+                chart.Series[0].Points.AddXY(u, points.Where(p => p == u).Count());
+            }
+
+            foreach (var p in NormalDistribution(points))
+            {
+                chart.Series[1].Points.AddXY(p.X, p.Y * points.Count());
+            }
+        }
+
+        private List<PointF> NormalDistribution(List<double> points, bool sample = false)
+        {
+            double mean = points.Average();
+            double std = Statistics.StandardDeviation(points);
+            double var = std * std;
+
+            List<PointF> n_points = new List<PointF>();
+            Normal dist = new Normal(mean, std);
+
+            foreach (var p in points.Distinct())
+            {
+                n_points.Add(new PointF((float)p, (float)dist.Density(p)));
+            }
+
+            return n_points.OrderBy(p => p.X).ToList();
+        }
+    }
+}
