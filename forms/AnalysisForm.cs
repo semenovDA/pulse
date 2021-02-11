20fb4c0c9c762f057125b615f2a81f790ab359dd
Dmitry <medtex03@gmail.com>
2021-02-11 11:31:29 +0800

    Universal form for Drag & Drop charts

---
+++ forms/AnalysisForm.cs
@@ -0,0 +1,119 @@
+﻿using pulse.collection;
+using pulse.core;
+using pulse.graphics;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace pulse.forms
+{
+    public partial class AnalysisForm : Form
+    {
+        Signal _signal;
+
+        bool mouse_is_down = false;
+        int _rows = 1;
+        int _columns = 1;
+
+        public AnalysisForm(Signal signal)
+        {
+            InitializeComponent();
+            _signal = signal;
+
+            Initialize();
+            workspaceSetup();
+        }
+
+        private void Initialize()
+        {
+            var histogram = new ListViewItem("Гисторграмма распределение");
+            histogram.Tag = "DISTRIBUTION_HISTOGRAM";
+            listView1.Items.Add(histogram);
+        }
+
+        private Chart GetChart(string chartname)
+        {
+            Chart chart = null;
+            switch(chartname)
+            {
+                case "DISTRIBUTION_HISTOGRAM":
+                    chart = new Histogram(_signal).chart;
+                    break;
+            }
+
+            return chart;
+        }
+        private Panel GetSegment()
+        {
+            Panel panel = new Panel();
+            panel.Dock = DockStyle.Fill;
+            panel.AllowDrop = true;
+            panel.DragDrop += workspace_DragDrop;
+            panel.DragEnter += workspace_DragEnter;
+            return panel;
+        }
+        private void workspaceSetup()
+        {
+            workspace.ColumnCount = _columns;
+            workspace.RowCount = _rows;
+
+            workspace.ColumnStyles.Clear();
+            workspace.RowStyles.Clear();
+            workspace.Controls.Clear();
+
+            workspace.BackColor = Color.White;
+            workspace.AutoSize = true;
+
+            for (int i = 0; i < _columns; i++) {
+                workspace.ColumnStyles.Add(
+                    new ColumnStyle(SizeType.Percent, 100 / _columns));
+            }
+            for (int i = 0; i < _rows; i++) {
+                workspace.RowStyles.Add(
+                    new RowStyle(SizeType.Percent, 100 / _rows));
+            }
+
+            for (int i = 0; i < _rows * _columns; i++) {
+                workspace.Controls.Add(GetSegment());
+            }
+        }
+        private void listView1_MouseDown(object sender, MouseEventArgs e)
+        {
+            mouse_is_down = true;
+        }
+        private void listView1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (!mouse_is_down || e.Button == MouseButtons.Right) return;
+            var selected_items = ((ListView)sender).SelectedItems;
+            if (selected_items.Count <= 0) return;
+            var next = selected_items.GetEnumerator();
+            next.MoveNext();
+            var str = ((ListViewItem)next.Current).Tag;
+            listView1.DoDragDrop(str, DragDropEffects.Copy | DragDropEffects.Move);
+        }
+        private void workspace_DragDrop(object sender, DragEventArgs e)
+        {
+            var chart = GetChart(e.Data.GetData(DataFormats.Text).ToString());
+            ((Control)sender).Controls.Add(chart);
+            mouse_is_down = false;
+        }
+        private void workspace_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.Text)) e.Effect = DragDropEffects.Copy;
+            else e.Effect = DragDropEffects.None;
+        }
+        private void RowsCount_ValueChanged(object sender, EventArgs e)
+        {
+            _rows = (int)RowsCount.Value;
+            workspaceSetup();
+        }
+        private void ColumnsCount_ValueChanged(object sender, EventArgs e)
+        {
+            _columns = (int)ColumnsCount.Value;
+            workspaceSetup();
+        }
+    }
+}
---
+++ forms/AnalysisForm.designer.cs
@@ -0,0 +1,240 @@
+﻿
+namespace pulse.forms
+{
+    partial class AnalysisForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AnalysisForm));
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ColumnsCount = new System.Windows.Forms.NumericUpDown();
+            this.RowsCount = new System.Windows.Forms.NumericUpDown();
+            this.vScrollBar1 = new System.Windows.Forms.VScrollBar();
+            this.mainPanel = new System.Windows.Forms.Panel();
+            this.workspace = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ColumnsCount)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.RowsCount)).BeginInit();
+            this.mainPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 2.724666F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 81.32832F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 16.0401F));
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel3, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.vScrollBar1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.mainPanel, 1, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(798, 528);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.AllowDrop = true;
+            this.tableLayoutPanel3.ColumnCount = 1;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.Controls.Add(this.listView1, 0, 0);
+            this.tableLayoutPanel3.Controls.Add(this.label1, 0, 2);
+            this.tableLayoutPanel3.Controls.Add(this.label2, 0, 5);
+            this.tableLayoutPanel3.Controls.Add(this.ColumnsCount, 0, 6);
+            this.tableLayoutPanel3.Controls.Add(this.RowsCount, 0, 3);
+            this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(672, 3);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 8;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 14F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 25F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 14F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 21F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 25F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 73F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(123, 522);
+            this.tableLayoutPanel3.TabIndex = 1;
+            // 
+            // listView1
+            // 
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(3, 3);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(117, 324);
+            this.listView1.TabIndex = 2;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.SmallIcon;
+            this.listView1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.listView1_MouseDown);
+            this.listView1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.listView1_MouseMove);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Location = new System.Drawing.Point(1, 345);
+            this.label1.Margin = new System.Windows.Forms.Padding(1);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(121, 18);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Количество строк:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label2.Location = new System.Drawing.Point(1, 404);
+            this.label2.Margin = new System.Windows.Forms.Padding(1);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(121, 19);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Количество столбцов:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // ColumnsCount
+            // 
+            this.ColumnsCount.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ColumnsCount.Location = new System.Drawing.Point(3, 427);
+            this.ColumnsCount.Maximum = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            this.ColumnsCount.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.ColumnsCount.Name = "ColumnsCount";
+            this.ColumnsCount.Size = new System.Drawing.Size(117, 20);
+            this.ColumnsCount.TabIndex = 5;
+            this.ColumnsCount.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.ColumnsCount.ValueChanged += new System.EventHandler(this.ColumnsCount_ValueChanged);
+            // 
+            // RowsCount
+            // 
+            this.RowsCount.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.RowsCount.Location = new System.Drawing.Point(3, 367);
+            this.RowsCount.Maximum = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            this.RowsCount.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.RowsCount.Name = "RowsCount";
+            this.RowsCount.Size = new System.Drawing.Size(117, 20);
+            this.RowsCount.TabIndex = 6;
+            this.RowsCount.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.RowsCount.ValueChanged += new System.EventHandler(this.RowsCount_ValueChanged);
+            // 
+            // vScrollBar1
+            // 
+            this.vScrollBar1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.vScrollBar1.Location = new System.Drawing.Point(2, 2);
+            this.vScrollBar1.Margin = new System.Windows.Forms.Padding(2);
+            this.vScrollBar1.Name = "vScrollBar1";
+            this.vScrollBar1.Size = new System.Drawing.Size(17, 524);
+            this.vScrollBar1.TabIndex = 1;
+            // 
+            // mainPanel
+            // 
+            this.mainPanel.Controls.Add(this.workspace);
+            this.mainPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mainPanel.Location = new System.Drawing.Point(24, 3);
+            this.mainPanel.Name = "mainPanel";
+            this.mainPanel.Size = new System.Drawing.Size(642, 522);
+            this.mainPanel.TabIndex = 0;
+            // 
+            // workspace
+            // 
+            this.workspace.AllowDrop = true;
+            this.workspace.ColumnCount = 1;
+            this.workspace.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.workspace.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.workspace.Location = new System.Drawing.Point(0, 0);
+            this.workspace.Name = "workspace";
+            this.workspace.RowCount = 1;
+            this.workspace.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.workspace.Size = new System.Drawing.Size(642, 522);
+            this.workspace.TabIndex = 0;
+            // 
+            // AnalysisForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(798, 528);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "AnalysisForm";
+            this.Text = "AnalysisForm";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel3.ResumeLayout(false);
+            this.tableLayoutPanel3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ColumnsCount)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.RowsCount)).EndInit();
+            this.mainPanel.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Panel mainPanel;
+        private System.Windows.Forms.VScrollBar vScrollBar1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.TableLayoutPanel workspace;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown ColumnsCount;
+        private System.Windows.Forms.NumericUpDown RowsCount;
+    }
+}
