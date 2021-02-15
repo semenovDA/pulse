c3d93923a5f5f94987545a256946c2462c79b9ae
Dmitry <medtex03@gmail.com>
2021-02-15 11:11:50 +0800

    Scatterogram chart component added

---
+++ graphics/Scatterogram.Designer.cs
@@ -0,0 +1,75 @@
+﻿
+namespace pulse.graphics
+{
+    partial class Scatterogram
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором компонентов
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chart)).BeginInit();
+            // 
+            // chart
+            // 
+            chartArea1.AxisX.ScrollBar.Enabled = false;
+            chartArea1.AxisY.ScrollBar.Enabled = false;
+            chartArea1.Name = "ChartArea1";
+            this.chart.ChartAreas.Add(chartArea1);
+            this.chart.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.chart.Location = new System.Drawing.Point(0, 0);
+            this.chart.Name = "chart";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.FastPoint;
+            series1.MarkerStyle = System.Windows.Forms.DataVisualization.Charting.MarkerStyle.Circle;
+            series1.Name = "Series1";
+            series2.BorderDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.Dot;
+            series2.BorderWidth = 3;
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series2.Name = "Series2";
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series3.Name = "Series3";
+            series3.YValuesPerPoint = 2;
+            this.chart.Series.Add(series1);
+            this.chart.Series.Add(series2);
+            this.chart.Series.Add(series3);
+            this.chart.Size = new System.Drawing.Size(768, 450);
+            this.chart.TabIndex = 0;
+            this.chart.Text = "chart1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart)).EndInit();
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.DataVisualization.Charting.Chart chart;
+    }
+}
---
+++ graphics/Scatterogram.cs
@@ -0,0 +1,78 @@
+﻿using pulse.collection;
+using pulse.forms;
+using System;
+using System.ComponentModel;
+using System.Linq;
+
+namespace pulse.graphics
+{
+    public partial class Scatterogram : Component
+    {
+        Signal _signal;
+
+        public Scatterogram(Signal signal)
+        {
+            InitializeComponent();
+            _signal = signal;
+            Initialize();
+        }
+        private void drawEllipse(double center, double h = 5, double w = 5, double angle = 0)
+        {
+            var rot = angle * Math.PI / 180.0;
+
+            for (int i = -180; i < 180; i++)
+            {
+                // TODO: check formula
+                var t = i * Math.PI / 180.0;
+                var x = w * Math.Cos(t) * Math.Cos(rot) - h * Math.Sin(t) * Math.Sin(rot) + center;
+                var y = h * Math.Sin(t) * Math.Cos(rot) + w * Math.Cos(t) * Math.Sin(rot) + center;
+                chart.Series[2].Points.AddXY(x, y);
+            }
+        }
+        private void Initialize()
+        {
+            var stats = _signal.computePoincare();
+            var peaks = _signal.computePeaks();
+
+            for (int i = 1; i < peaks.Length - 1; i++)
+            {
+                int x = Math.Abs(peaks[i] - peaks[i - 1]);
+                int y = Math.Abs(peaks[i] - peaks[i + 1]);
+                chart.Series[0].Points.AddXY(x, y);
+            }
+
+            var max_x = chart.Series[0].Points.Max(s => s.XValue) + 20;
+            var max_y = chart.Series[0].Points.Max(s => s.YValues[0]) + 20;
+
+            var min_x = chart.Series[0].Points.Min(s => s.XValue) - 20;
+            var min_y = chart.Series[0].Points.Min(s => s.YValues[0]) - 20;
+
+            chart.Series[1].Points.AddXY(0, 0);
+            chart.Series[1].Points.AddXY(max_x, max_y);
+
+            chart.ChartAreas[0].AxisX.ScaleView.Zoom(min_x, max_x);
+            chart.ChartAreas[0].AxisY.ScaleView.Zoom(min_y, max_y);
+
+            chart.ChartAreas[0].AxisX.Title = "RR (ms)";
+            chart.ChartAreas[0].AxisY.Title = "RR[n-1] (ms)";
+
+            // TODO: Fix double first
+            var sd1 = stats["sd1"].ToObject<double>();
+            var sd2 = stats["sd2"].ToObject<double>();
+
+            var center = chart.Series[0].Points
+                .Where(s => s.YValues[0] == s.XValue)
+                .Select(s => s.XValue)
+                .Average();
+
+            drawEllipse(center, sd1, sd2);
+        }
+        public void Show(string title = "Скатерграмма")
+        {
+            var emptyFrom = new Empty();
+            emptyFrom.Text = title;
+            emptyFrom.workspace.Controls.Add(chart);
+            emptyFrom.Show();
+        }
+    }
+}
