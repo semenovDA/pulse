c660b7b5ae957ccc257e205fc224ec51a260f365
Dmitry <medtex03@gmail.com>
2021-02-15 11:10:01 +0800

    Dummy form for graphics.* charts

---
+++ forms/Empty.Designer.cs
@@ -0,0 +1,61 @@
+﻿
+namespace pulse.forms
+{
+    partial class Empty
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Empty));
+            this.workspace = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // workspace
+            // 
+            this.workspace.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.workspace.Location = new System.Drawing.Point(0, 0);
+            this.workspace.Name = "workspace";
+            this.workspace.Size = new System.Drawing.Size(800, 450);
+            this.workspace.TabIndex = 0;
+            // 
+            // Empty
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.workspace);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Empty";
+            this.Text = "Chart";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.Panel workspace;
+    }
+}
